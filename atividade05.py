38985fb1b49348250226ae7cb93ebfa39c50826d
vitorlopes1248 <vitorlopes1248@gmail.com>
2024-09-04 15:35:05 -0300

    atividade05 minutos para segundos

---
+++ atividade05.py
@@ -0,0 +1,6 @@
+# Escreva um programa que receba um valor em minutos e converta para segundos.
+minutos = int(input("quantos minutos serão convertidos: "))
+
+converter = minutos * 60
+
+print("valor em segundo: ", converter)
